fd2c0542fd22e336698dbe25e370d3959d34b79c
DamianoCasolari <damianocasolari@gmail.com>
2023-05-30 17:39:03 +0200

    little fix t

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
 @extends('layout.app')
 @section('content')
     <div class="container">
-        <div class="bg-light p-4 row row-cols-2 row-cols-lg-3">
+        <div class="bg_night p-4 row row-cols-2 row-cols-lg-3">
             @foreach ($comics as $comic)
                 <div class="p-3">
-                    <div class="card my_card p-4 col gutter-3">
+                    <div class="card my_card p-4 col shadow">
                         <div class="container_img d-flex justify-content-center">
                             <img src="{{ $comic->thumb }}" class="comic_img w-100" alt="{{ $comic->title . 'image' }}">
                         </div>
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column justify-content-between">
                             <p class="card-text">{{ $comic->type }}</p>
-                            <h6 class="card-subtitle mb-2 text-muted "><b> Series :</b> <br>
-                                <span class="ms-3">{{ $comic->series }}</span>
+                            <h6 class="card-subtitle mb-2 text-muted"><b> Series :</b> <br>
+                                <span class="ms-3 ">{{ $comic->series }}</span>
                             </h6>
                             <h6 class="card-subtitle mb-2 text-muted "><b> Sale date :</b> <br>
                                 <span class="ms-3">{{ $comic->sale_date }}</span>
                             </h6>
 
-                            <h5 class="card-title mt-4">{{ $comic->price }}</h5>
+                            <hr>
+                            <h5 class="card-title text-end">{{ $comic->price }}</h5>
                         </div>
                     </div>
                 </div>
